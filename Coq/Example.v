5960fb4c9db43a40b2d7991ef4ff540a373e67f5
ipsissima <roamruls@gmail.com>
2025-06-28 01:22:52 +0200

    Create Example.v

---
+++ Coq/Example.v
@@ -0,0 +1,67 @@
+Require Import Coq.Reals.Reals.
+Require Import Coq.QArith.QArith.
+Require Import Coq.QArith.Qreals.
+Require Import Coq.Lists.List.
+Open Scope R_scope.
+Import ListNotations.
+
+Definition inject_Q := Q2R.
+
+(* Example basis: B-splines, here abstracted *)
+Parameter basis : nat -> (R -> R).
+
+(* Define f(x) = sin(pi x) *)
+Definition f_example (x : R) : R := sin (PI * x).
+
+(* Example: 10 basis functions *)
+Definition index_set : list nat := [0;1;2;3;4;5;6;7;8;9]%nat.
+
+(* Example coefficient list for sin(pi x) with B-splines *)
+Definition coeff_list : list Q :=
+  [0.5283452;  -0.3156781;  0.1023417;  -0.0456123;
+   0.0145789;  -0.0051234; 0.0012345; -0.0003126;
+   0.0000789; -0.0000192]%Q.
+
+Lemma coeffs_length : length coeff_list = length index_set.
+Proof. reflexivity. Qed.
+
+(* Local certificate on [0,1] *)
+Record LocalCertificate := {
+  indices : list nat;
+  coeffs  : list Q;
+  coeffs_length_pf : length coeffs = length indices
+}.
+
+Definition local_cert : LocalCertificate :=
+  {| indices := index_set;
+     coeffs := coeff_list;
+     coeffs_length_pf := coeffs_length |}.
+
+(* Global certificate: just one interval for this example *)
+Record GlobalCertificate := {
+  subintervals : list (R * R);
+  locals : list LocalCertificate;
+  local_match : length subintervals = length locals
+}.
+
+Definition global_cert : GlobalCertificate :=
+  {| subintervals := [(0,1)];
+     locals := [local_cert];
+     local_match := eq_refl |}.
+
+(* Approximant function *)
+Fixpoint map2 {A B C} (f : A -> B -> C) (l1 : list A) (l2 : list B) : list C :=
+  match l1, l2 with
+  | a::l1', b::l2' => f a b :: map2 f l1' l2'
+  | _, _ => @nil C
+  end.
+
+Definition f10 (x : R) : R :=
+  fold_right Rplus 0
+    (map2 (fun idx aQ => (inject_Q aQ) * (basis idx x)) index_set coeff_list).
+
+(* For full pipeline, could now reference error bound etc. *)
+
+(* Example: W^{1,2} error estimate, provided externally *)
+Parameter W12_error : R.
+Axiom error_bound_example : W12_error < 0.001.
