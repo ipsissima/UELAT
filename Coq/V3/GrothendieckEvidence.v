34f136608fdbc9bfdff9acc7226cdc0b1e80d6a4
andreu-toposcircuitry <andreuballus@toposcircuitry.com>
2026-08-27 20:33:44 +0200

    Make indexed functor laws implicit under Rocq 9.2

--- Coq/V3/GrothendieckEvidence.v
+++ Coq/V3/GrothendieckEvidence.v
@@ -43,6 +43,8 @@ Arguments fibre_hom {B} _ _ _ _.
 Arguments fibre_id {B} _ _ _.
 Arguments push_obj {B s x y} _ _.
 Arguments push_hom {B s x y} _ _.
+Arguments push_id {B} s x a.
+Arguments push_comp {B} s x y z f g a.
 
 Definition transport_source
     {A : Type} (P : A -> Type) {x y : A}
