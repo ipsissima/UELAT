6791ce1ddd1bc2545d9f6b8561890b3203bae68d
andreu-toposcircuitry <andreuballus@toposcircuitry.com>
2026-08-27 19:57:32 +0200

    Construct executable bounded functional on searchable rational core

---
+++ Coq/V3/SearchableCoordinateCoreFunctional2.v
@@ -0,0 +1,109 @@
+(** SearchableCoordinateCoreFunctional2.v -- executable dense-core functional
+    from an admissible dual coordinate point. *)
+
+From Coq Require Import Reals QArith Qreals Lra Ring Field.
+From UELAT.V3 Require Import
+  CertificateEnrichment ComputableBanach SearchableCore
+  NormalizedCoreCoordinates CoordinateDualBall CoordinateFunctionalGraph.
+
+Module UELAT_V3_SearchableCoordinateCoreFunctional2.
+Import UELAT_V3_CertificateEnrichment.
+Import UELAT_V3_ComputableBanach.
+Import UELAT_V3_SearchableCore.
+Import UELAT_V3_NormalizedCoreCoordinates.
+Import UELAT_V3_CoordinateDualBall.
+Import UELAT_V3_CoordinateFunctionalGraph.
+
+Section DirectCoreFunctional.
+  Variable S : SearchableCorePresentation.
+  Let B := sc_banach S.
+  Variable a : CoordinateDualBallPoint B.
+
+  Definition direct_core_value (p : core_code B) : R :=
+    Q2R (core_scale_factor B p) * cdb_coordinates a (core_index S p).
+
+  Lemma normalized_index_for_code : forall p,
+    normalized_core B (core_index S p)
+      = core_decode (normalized_core_code B p).
+  Proof.
+    intro p. unfold normalized_core. rewrite core_index_correct. reflexivity.
+  Qed.
+
+  Lemma rescale_normalized_code : forall p,
+    cb_scale B (Q2R (core_scale_factor B p))
+      (core_decode (normalized_core_code B p)) = core_decode p.
+  Proof.
+    intro p. unfold normalized_core_code.
+    rewrite core_scale_sound, cb_scale_assoc.
+    rewrite Q2R_inv by apply core_scale_factor_nonzero_Q.
+    pose proof (core_scale_factor_real_positive B p) as Hpos.
+    replace
+      (Q2R (core_scale_factor B p) * / Q2R (core_scale_factor B p))
+      with 1 by (field; lra).
+    apply cb_scale_one.
+  Qed.
+
+  Theorem direct_core_value_in_span_graph : forall p,
+    CoordinateSpanGraph B a (core_decode p) (direct_core_value p).
+  Proof.
+    intro p.
+    unfold CoordinateSpanGraph.
+    destruct (normalized_core_rationally_spans_original_core B p) as [i [c Hpc]].
+    (** Use the explicit searchable index rather than an arbitrary spanning
+        index, so the value is executable. *)
+    exists [(core_scale_factor B p, core_index S p)].
+    split.
+    - unfold span_vector. simpl.
+      rewrite core_add_sound, core_scale_sound, core_zero_sound, cb_add_zero_r.
+      rewrite normalized_index_for_code.
+      apply rescale_normalized_code.
+    - unfold span_value, direct_core_value. simpl.
+      change (Q2R (core_scale_factor B p) * cdb_coordinates a (core_index S p) + 0
+              = Q2R (core_scale_factor B p) * cdb_coordinates a (core_index S p)).
+      ring.
+  Qed.
+
+  Theorem direct_core_value_bounded : forall p,
+    Rabs (direct_core_value p) <= cb_norm B (core_decode p).
+  Proof.
+    intro p. eapply coordinate_span_graph_bounded.
+    apply direct_core_value_in_span_graph.
+  Qed.
+
+  Theorem direct_core_value_zero : direct_core_value (core_zero B) = 0.
+  Proof.
+    eapply coordinate_span_graph_functional.
+    - apply direct_core_value_in_span_graph.
+    - unfold CoordinateSpanGraph. exists []. split.
+      + rewrite span_vector_nil, core_zero_sound. reflexivity.
+      + rewrite span_value_nil. reflexivity.
+  Qed.
+
+  Theorem direct_core_value_add : forall p q,
+    direct_core_value (core_add B p q) = direct_core_value p + direct_core_value q.
+  Proof.
+    intros p q.
+    eapply coordinate_span_graph_functional.
+    - apply direct_core_value_in_span_graph.
+    - unfold CoordinateSpanGraph.
+      destruct (direct_core_value_in_span_graph p) as [tp [Hvp Hrp]].
+      destruct (direct_core_value_in_span_graph q) as [tq [Hvq Hrq]].
+      exists (tp ++ tq). split.
+      + rewrite span_vector_append, <- Hvp, <- Hvq, core_add_sound. reflexivity.
+      + rewrite span_value_append, <- Hrp, <- Hrq. reflexivity.
+  Qed.
+
+  Theorem admissible_coordinate_gives_bounded_rational_core_functional :
+    (forall p, Rabs (direct_core_value p) <= cb_norm B (core_decode p))
+    /\ direct_core_value (core_zero B) = 0
+    /\ (forall p q,
+      direct_core_value (core_add B p q) = direct_core_value p + direct_core_value q).
+  Proof.
+    repeat split.
+    - apply direct_core_value_bounded.
+    - apply direct_core_value_zero.
+    - apply direct_core_value_add.
+  Qed.
+End DirectCoreFunctional.
+
+End UELAT_V3_SearchableCoordinateCoreFunctional2.
